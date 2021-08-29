f842da21e9d67d4ad8e127acbdf30f6b68edafc5
rodrigogomesrc <rocha.rodrigogomes@gmail.com>
2021-08-29 15:15:02 -0300

    added game component inicial template

--- KanyeOrBible/src/Components/Game.svelte
+++ KanyeOrBible/src/Components/Game.svelte
@@ -1,9 +1,9 @@
 <script>
     import { getKanyeSentence, getBibleSentence } from '../Services/api';
 
+    let statement = "Statement";
 	/*
 	getBibleSentence(function(data){
-		console.log("mostrando os dados aqui recebidos: ");
 		console.log(data);
 	})
 
@@ -12,6 +12,43 @@
 	})
 	*/
 
+    function onVote(option){
+        console.log(option);
+    }
+
 </script>
 
-<p>Game Component</p>
+<div class="container">
+    <div>
+        <h2 class="statement">{statement}</h2>
+    </div>
+    <div class="button-container">
+        <button on:click={()=>onVote("bible")}>Bible</button>
+        <button on:click={()=>onVote("kanye")}>Kanye</button>
+    </div>
+</div>
+
+<style>
+    .container{
+        width: 700px;
+        border: 1px solid black;
+        margin: 0 auto;
+        background-color: aliceblue;
+        padding: 20px;
+    }
+
+    .statement {
+        text-align: center;
+    }
+
+    .button-container {
+        display: flex;
+        justify-content: center;
+    }
+
+    button {
+        margin: 5px;
+        width: 100px;
+    }
+
+</style>
