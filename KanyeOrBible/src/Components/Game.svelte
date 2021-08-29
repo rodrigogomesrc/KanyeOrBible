a10cddce0aff325374acd2f48e7bf9b444601d92
rodrigogomesrc <rocha.rodrigogomes@gmail.com>
2021-08-29 14:40:32 -0300

    add apis requests

--- KanyeOrBible/src/Components/Game.svelte
+++ KanyeOrBible/src/Components/Game.svelte
@@ -1,4 +1,16 @@
 <script>
+    import { getKanyeSentence, getBibleSentence } from '../Services/api';
+
+	/*
+	getBibleSentence(function(data){
+		console.log("mostrando os dados aqui recebidos: ");
+		console.log(data);
+	})
+
+	getKanyeSentence(function(data){
+		console.log("api do kanye chamada");
+	})
+	*/
 
 </script>
 
--- KanyeOrBible/src/Services/api.js
+++ KanyeOrBible/src/Services/api.js
@@ -1,1 +1,25 @@
 /* Make the api's requests here*/
+import $ from "jquery";
+
+const bibleApiUrl = 'https://labs.bible.org/api/?passage=random&type=json';
+const kanyeApiUrl = 'https://api.kanye.rest';
+
+export function getKanyeSentence(callback){
+    $.ajax({
+        url: kanyeApiUrl, 
+        success: function(result){
+            callback(result.quote);
+        }
+    });
+}
+
+export function getBibleSentence(callback) {
+    $.ajax({
+        url: bibleApiUrl, 
+        crossDomain: true,
+        dataType: 'jsonp',
+        success: function(result){
+            callback(result[0].text.split("<a")[0]);
+        }
+    });
+}
