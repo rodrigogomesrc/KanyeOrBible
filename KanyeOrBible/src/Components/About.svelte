918ec8a036e463560bca73da4235acbb17746efb
rodrigogomesrc <rocha.rodrigogomes@gmail.com>
2021-09-04 18:44:09 -0300

    about draft page

--- KanyeOrBible/src/Components/About.svelte
+++ KanyeOrBible/src/Components/About.svelte
@@ -2,4 +2,27 @@
 
 </script>
 
-<p>About Component</p>
+<div class="about-container">
+    <div>
+       <h2>About</h2>
+       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Nullam elit enim, mollis eu lacus id, facilisis eleifend purus. 
+        </p>
+    </div>
+</div>    
+    
+<style>
+
+    .about-container {
+        width: 800px;
+        margin: 0 auto;
+        padding: 20px;
+    }
+
+    @media only screen and (max-width: 700px) {
+        .about-container {
+            width: calc(100% -40px);
+        }
+    }
+
+</style>
