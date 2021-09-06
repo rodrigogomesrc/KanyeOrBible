3fbc5491238c754d8f2f28c0da5d59f4ed773947
Gabriel Reis Andrade <joaogabriel2@gmail.com>
2021-09-06 11:30:54 -0300

    Populated about page

--- KanyeOrBible/src/Components/About.svelte
+++ KanyeOrBible/src/Components/About.svelte
@@ -2,27 +2,21 @@
 
 </script>
 
-<div class="about-container">
-    <div>
-       <h2>About</h2>
-       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Nullam elit enim, mollis eu lacus id, facilisis eleifend purus. 
-        </p>
+<div class="container">
+    <div class="center-container">
+        <h2 class="center-text highlighted-text" id="about-title">About</h2>
+        <p class="center-text" id="about-description">Kanye or Bible is a small game where the player must determine if a phrase is from Kanye West or from the Bible.</p>
+        <div class="center-text" id="about-madeby">Made by:</div>
+        <div class="center-text" id="about-digao">Rodrigo Gomes</div>
+        <div class="center-text" id="about-reis">Gabriel Andrade</div>
     </div>
-</div>    
+</div>   
     
 <style>
-
-    .about-container {
-        width: 800px;
-        margin: 0 auto;
-        padding: 20px;
+    #about-madeby{
+        font-size:x-large;
     }
-
-    @media only screen and (max-width: 700px) {
-        .about-container {
-            width: calc(100% -40px);
-        }
+    #about-digao,#about-reis{
+        font-size: medium;
     }
-
 </style>
