7f8deb08233cfcdb0776b300bc22dd7a75d044f1
Gabriel Reis Andrade <joaogabriel2@gmail.com>
2021-09-06 11:44:44 -0300

    Changes to about text

--- KanyeOrBible/src/Components/About.svelte
+++ KanyeOrBible/src/Components/About.svelte
@@ -6,6 +6,8 @@
     <div class="center-container">
         <h2 class="center-text highlighted-text" id="about-title">About</h2>
         <p class="center-text" id="about-description">Kanye or Bible is a small game where the player must determine if a phrase is from Kanye West or from the Bible.</p>
+        <div class="center-text" id="about-howtoplay">How to play:</div>
+        <p class="center-text" id="about-howtoplaytext">After starting the game, try to get a high score by answering the questions correctly taking as little time as possible.</p>
         <div class="center-text" id="about-madeby">Made by:</div>
         <div class="center-text" id="about-digao">Rodrigo Gomes</div>
         <div class="center-text" id="about-reis">Gabriel Andrade</div>
@@ -13,7 +15,7 @@
 </div>   
     
 <style>
-    #about-madeby{
+    #about-madeby,#about-howtoplay{
         font-size:x-large;
     }
     #about-digao,#about-reis{
